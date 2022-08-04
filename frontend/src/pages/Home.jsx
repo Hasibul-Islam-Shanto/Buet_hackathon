2318a6105f4646b1fb8a705c98e33c4cd149bd6f
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2022-08-04 13:46:58 +0600

    voice to text added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import Box from "@mui/material/Box";
+import SearchandMic from "../components/Home/SearchandMic";
 const Home = () => {
   return (
     <>
-      <h1>Home page..</h1>
+      <Box
+        sx={{
+          height: "100vh",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          //   justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <SearchandMic />
+      </Box>
     </>
   );
 };
